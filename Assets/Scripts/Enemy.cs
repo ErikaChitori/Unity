c27038a17e62500c2109287a049e0f5024e7fad0
ErikaChitori <903965996@qq.com>
2021-06-05 13:51:12 +0800

    2020.6.5 Audio Update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     protected Animator anim;
+    protected AudioSource deathAudio;
     // Start is called before the first frame update
     protected virtual void Start()
     {
         anim=GetComponent<Animator>();
+        deathAudio=GetComponent<AudioSource>();
     }
     public void Death()
     {
+        
         Destroy(gameObject);
     }
     public void JumpOn()
     {
+        deathAudio.Play();
         anim.SetTrigger("Death");
     }
 }
